240b17464e06ff98e283fbb087acb3328227d47b
Die1060 <92213333+Die1060@users.noreply.github.com>
2022-05-24 14:13:19 -0600

    Update palabras.js
    agregar arreglo de palabras

--- palabras.js
+++ palabras.js
@@ -1,15 +1,11 @@
     let arregloPalabras = {
-    "l" :["limpio", "lenguaje" , "lector"],
-    "m" : ["maximizar", "microprocesador" , "modulo" , "multimedia"],
-    "n" : ["navegador", "nodo" , "nucleo"],
-    "o" : ["ordenador", "online" , "ordenado"],
-    "p" : ["pendrive", "pixel" , "procesador" , "plugin"],
-    "r" :  ["resolucion", "router" , "red" , "respaldo"],
-    "s" : ["servidor", "seguridad" , "software" , "sistema"],
-    "t" : ["tarjeta", "tipografia" , "teclado", "tablet"],
-    "u" : ["unidad", "usabilidad" , "usuario"],
-    "v" : ["vector", "videoconferencia" , "virtual"],
-    "w" : ["website", "webcam" , "window"],
-
+    "a":["analítico", "amable", "alegre"], "b":["bendecido","bailador","bárbaro"], "c":["caballeroso", "calculador", "callado"], 
+    "d":["divino","diseñador","decorativo"],  "e":["elegante","entuciasta","esmerado"], "f":["fantástico", "famoso", "fascinante"], 
+    "g":["gallardo","ganador","garante"],  "h":["hábil","hidalgo","hacendado"], "i":["idealista", "individual", "imitador"], 
+    "j":["jurador","juvenil","joven"],  "k":["kaki","Kurdo","Keynesiano"], "l" :["limpio", "lenguaje" , "lector"],
+    "m" : ["maximizar", "microprocesador" , "modulo" , "multimedia"], "n" : ["navegador", "nodo" , "nucleo"],
+    "o" : ["ordenador", "online" , "ordenado"], "p" : ["pendrive", "pixel" , "procesador" , "plugin"],
+    "r" :  ["resolucion", "router" , "red" , "respaldo"], "s" : ["servidor", "seguridad" , "software" , "sistema"],
+    "t" : ["tarjeta", "tipografia" , "teclado", "tablet"], "u" : ["unidad", "usabilidad" , "usuario"],
+    "v" : ["vector", "videoconferencia" , "virtual"], "w" : ["website", "webcam" , "window"],
     }
-    
