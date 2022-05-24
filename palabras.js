1293cd4df12431b24a26804efb64749f8d4bdf41
LAPTOP-QN8K0DCO\claudia soza <cldrms8@gmail.com>
2022-05-24 22:07:23 +0200

    inicio de arreglo de palabras para recordar las que ingresaste

---
+++ palabras.js
@@ -0,0 +1,15 @@
+    let arregloPalabras = {
+    "l" :["limpio", "lenguaje" , "lector"],
+    "m" : ["maximizar", "microprocesador" , "modulo" , "multimedia"],
+    "n" : ["navegador", "nodo" , "nucleo"],
+    "o" : ["ordenador", "online" , "ordenado"],
+    "p" : ["pendrive", "pixel" , "procesador" , "plugin"],
+    "r" :  ["resolucion", "router" , "red" , "respaldo"],
+    "s" : ["servidor", "seguridad" , "software" , "sistema"],
+    "t" : ["tarjeta", "tipografia" , "teclado", "tablet"],
+    "u" : ["unidad", "usabilidad" , "usuario"],
+    "v" : ["vector", "videoconferencia" , "virtual"],
+    "w" : ["website", "webcam" , "window"],
+
+    }
+    
